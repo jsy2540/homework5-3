e4aee3d9a8f2506a4e93a7dd633cfa3890286a9b
201344033 <jsy2540@naver.com>
2016-10-20 23:30:57 +0900

    수정중

--- src/ExamForWhileStars.java
+++ src/ExamForWhileStars.java
@@ -6,12 +6,11 @@ public class ExamForWhileStars {
 		int flag = 0;
 		
 		while(flag != 6){
-			System.out.println("1. 1부터 입력한 수 더하기.");
-			System.out.println("2. 최대값/최소값구하기");
-			System.out.println("3. 입력받은숫자의 합계와 평균구하기");
-			System.out.println("4. 원하는 구구단 출력하기 ");
-			System.out.println("5. 짝수단/홀수단 출력하기");
-			System.out.println("6. 종료하기.");
+			System.out.println("1. 정사각형 벌찍기");
+			System.out.println("2. 직각삼각 별찎기");
+			System.out.println("3. 이등변삼각형 별찍기");
+			System.out.println("4. 다이아몬드 별찍기");
+			System.out.println("5. 종료하기");
 			
 			System.out.print("원하는 메뉴 : ");
 			flag = in.nextInt();
@@ -58,19 +57,23 @@ class RectagleStars{
 		Scanner in = new Scanner(System.in); 
 	    System.out.print("1부터 입력한 수까지 더하기 마지막 수를 입력(Q:종료) : "); 
 	
-	    String input = in.nextLine();
-	    if(input.equals("Q")){
-	    	return;
-	    }
-	    int numbers = Integer.parseInt(input);
-	    int result =0; 
-	    for(int i = 1; i <= numbers; i++){ 
-	        result = result + i;
-	    }
-	    System.out.print("총 합은 "+result+"입니다.\n"); 
-	    return;
-}
-}
+		  System.out.println("1번메뉴 클래스 :RectagleStars");
+		  System.out.println("=======================================");
+
+		  System.out.println();
+		  System.out.print("정서각형 크기입력 :");
+		  int a = in.nextInt();
+
+		  
+		  for(int i = 0; i<a; i++)  
+		   {    
+		     for(int j = 1; j<=i; j++)    
+		       System.out.print("*");     
+		     System.out.println();
+		   } 
+		  System.out.println();	  
+		 }
+		}
 
 class TriangleStars{
 	static void TS() {
@@ -108,22 +111,29 @@ class IsoscelesTriangleStars{
 	static void ITS() {
 		Scanner in = new Scanner(System.in); 
 		
-		int i, sum = 0;
-		double avg = 0;
-	    int array[] = new int[5];
-	    for(i = 0; i < 5; i++)
-	    {
-	    	System.out.print("숫자를  입력(Q:종료) : ");
-	        array[i] = in.nextInt();
-	        sum += array[i]; 
-	    }
-	   
-	    avg = (double) sum / array.length;
-	    
-	    System.out.printf("합계는  %d 이고, 평균은  %.2f 입니다.\n", sum,avg );
-	    return;	    
-	    }
-	}
+
+		  System.out.println("3.3번메뉴 클래스 :IsoscelesTriangleStars");
+		  System.out.println("=======================================");
+
+		  System.out.println();
+		  System.out.print("아둥변삼각형 높이입력 :");
+		  int a = in.nextInt();
+
+		  
+		  for(int i = 0; i<a; i++)  
+		   {
+		     for(int j = a; j>i; j--)     
+		       System.out.print(" ");          
+		   for(int j = i; j>=0; j--)     
+		       System.out.print("*");     
+		     for(int j = 1; j<=i; j++)    
+		       System.out.print("*");     
+		     System.out.println();
+		   }
+		  
+		  System.out.println();	  
+		 }
+		}
 
 class DiamondStars{
 	static void DS() {
