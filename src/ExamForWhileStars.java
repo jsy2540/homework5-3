441ce5aba9eda79b8a6cec35613688c6c7ab2759
201344033 <jsy2540@naver.com>
2016-10-20 23:48:09 +0900

    수정 중~

--- src/ExamForWhileStars.java
+++ src/ExamForWhileStars.java
@@ -7,7 +7,7 @@ public class ExamForWhileStars {
 		
 		while(flag != 6){
 			System.out.println("1. 정사각형 벌찍기");
-			System.out.println("2. 직각삼각 별찎기");
+			System.out.println("2. 직각삼각 별찍기");
 			System.out.println("3. 이등변삼각형 별찍기");
 			System.out.println("4. 다이아몬드 별찍기");
 			System.out.println("5. 종료하기");
@@ -28,8 +28,8 @@ public class ExamForWhileStars {
 				case 4:
 					DiamondStars.DS();
 					break;
-				default:
-					System.out.println("다시 입력 하시오.");
+				case 5:
+					System.exit(0);
 					break;
 			}
 		}
@@ -55,19 +55,17 @@ class RectagleStars{
 	static void RS() 
 	{
 		Scanner in = new Scanner(System.in); 
-	    System.out.print("1부터 입력한 수까지 더하기 마지막 수를 입력(Q:종료) : "); 
 	
 		  System.out.println("1번메뉴 클래스 :RectagleStars");
 		  System.out.println("=======================================");
 
 		  System.out.println();
-		  System.out.print("정서각형 크기입력 :");
+		  System.out.print("정사각형 크기입력 :");
 		  int a = in.nextInt();
 
-		  
-		  for(int i = 0; i<a; i++)  
+		  for(int i = 0; i<6; i++)  
 		   {    
-		     for(int j = 1; j<=i; j++)    
+		     for(int j =1 ; j<=i; j++)    
 		       System.out.print("*");     
 		     System.out.println();
 		   } 
@@ -79,40 +77,33 @@ class TriangleStars{
 	static void TS() {
 		Scanner in = new Scanner(System.in); 
 		
-	    int i, j, max, min;
-	    int array[] = new int[6];
-	    String str; 
-	    
-	    for(i = 0; i < 6; i++)
-	    {
-	    	System.out.print("0~100사이의 숫자를  입력(Q:종료) : "); 
-	    	 array[i] = in.nextInt(); 
-	    }
-	   
-	    max = array[0];
-	    min = array[0];
-	    
-	    for (j=0; j<6; j++){
-	    	if(max < array[j]){
-	    		max = array[j];
-	    	}
-	    }
-	    for (j=0; j<6; j++){
-	    	if(min > array[j]){
-	    	   min = array[j];
-	    	}
-	    }
-	    System.out.println("가장 큰 수는  "+max+"이고, 가장 작은 수는 "+min+"입니다."  );   
-	    return;
+		  System.out.println("2번메뉴 클래스 :TriangleStars");
+		  System.out.println("=======================================");
+
+		  System.out.println();
+		  System.out.print("직각삼각형의 왼쪽여백과 높이입력 :");
+		  int a = in.nextInt();
+
+		  
+		  for(int i=0;i<6;i++){
+			   for(int j=1;j<=a-i;j++){
+			    System.out.print(" ");
+			   }
+			   for(int j=1;j<=i;j++){
+			    System.out.print("*");
+			   }
+			  System.out.println();
+			  }
 	}
 }
+
 
 class IsoscelesTriangleStars{
 	static void ITS() {
 		Scanner in = new Scanner(System.in); 
 		
 
-		  System.out.println("3.3번메뉴 클래스 :IsoscelesTriangleStars");
+		  System.out.println("3번메뉴 클래스 :IsoscelesTriangleStars");
 		  System.out.println("=======================================");
 
 		  System.out.println();
@@ -138,20 +129,34 @@ class IsoscelesTriangleStars{
 class DiamondStars{
 	static void DS() {
 		Scanner in = new Scanner(System.in);
-		System.out.printf("출력하고 싶은 단(Q:종료)" );
-		int i = in.nextInt();
-		
-		if(i < 1 && i > 10){
-			System.out.printf("잘못입력");	
-			return;
-		}	
-		for(int j = 1; j < 10; j++){			
-			System.out.println(i + " * " + j + " = " + i*j);
+		  System.out.println("4번메뉴 클래스 :DiamondStars");
+		  System.out.println("=======================================");
+		  System.out.println();
+		  
+		  System.out.print("다이아몬드의 크기입력  : ");   
+		  int num = in.nextInt();
+		  int i, j, k, l;
+		  for(i=0; i<num; i++) 
+		  {
+		   for(j=num-1; j>i; j--) 
+		    System.out.print(" ");   
+		   for(k=0; k<=i; k++) 
+		    System.out.print("*");
+		   for(l=0; l<i; l++)  
+		    System.out.print("*");
+		   System.out.println(); 
+		  }
+		  
+		  for(i=0; i<num-1; i++)  
+		  {
+		   for(j=0; j<=i; j++) 
+		    System.out.print(" ");   
+		   for(k=num-1; k>i; k--) 
+		        System.out.print("*");
+		   for(l=num-2; l>i; l--) 
+		        System.out.print("*");
+		   System.out.println(); 
+		  }
+		  
+		  }
 		}
-	
-
-		    	System.out.println("");
-		    	   
-	}
-
-}
